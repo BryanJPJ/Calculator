e32821b8b45cbb4637c9a151856563070a81b3fa
Bryan <bryanpjpgi2b@hotmail.com>
2022-12-14 09:33:29 +0100

    doing calculator

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue';
+
+let showOnDisplay = "";
+function addDisplay(element) {
+  showOnDisplay += element
+}
+const textDisplay = computed(() => {
+  return showOnDisplay
+}
+)
+</script>
+
+<template>
+ <main>
+  <p id="display">{{textDisplay}}</p> 
+  <button id="oneKey" @click="addDisplay(1)">1</button>
+ </main>
+</template>
+
+<style scoped>
+button {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid black ;
+}
+</style>
