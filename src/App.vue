6a75798773a2715913c7879d56176f99de0509a5
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-12-15 09:42:13 +0100

    Cambio de turno

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,53 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 
-let showOnDisplay = "";
+let textOnDisplay = ref("");
+let showOnDisplay = ref(0);
 
-const textDisplay = computed(() => {
-  return showOnDisplay !== "" ? "Psatata" : 0
+const showDisplay = computed({
+  get() {
+    return showOnDisplay.value
+  }
 })
 
-function addDisplay(element) {
-  showOnDisplay += element
-  return showOnDisplay
+
+const addToDisplay = function (element) {
+  if (element == "c") {
+    showOnDisplay.value = 0;
+    textOnDisplay.value = "";
+    return
+  }
+  if (element == "=") {
+    showOnDisplay.value = eval(showOnDisplay.value);
+    textOnDisplay.value = showOnDisplay.value;
+    return
+  }
+  textOnDisplay.value += element;
+  showOnDisplay.value = textOnDisplay.value;
 }
 
 </script>
 
 <template>
- <main>
-  <p id="display">{{ textDisplay }}</p> 
-  <button id="oneKey" @click="addDisplay(1)">1</button>
- </main>
-</template>
+<main>
+    <p id="display">{{ showDisplay }}</p>
+    <button id="oneKey" @click="addToDisplay(1)">1</button>
+    <button id="twoKey" @click="addToDisplay(2)">2</button>
+    <button id="threeKey" @click="addToDisplay(3)">3</button>
+    <button id="fourKey" @click="addToDisplay(4)">4</button>
+    <button id="fiveKey" @click="addToDisplay(5)">5</button>
+    <button id="sixKey" @click="addToDisplay(6)">6</button>
+    <button id="sevenKey" @click="addToDisplay(7)">7</button>
+    <button id="heightKey" @click="addToDisplay(8)">8</button>
+    <button id="nineKey" @click="addToDisplay(9)">9</button>
+    <button id="zeroKey" @click="addToDisplay(0)">0</button>
+    <button id="additionKey" @click="addToDisplay(`+`)">+</button>
+    <button id="subtractionKey" @click="addToDisplay(`-`)">-</button>
+    <button id="multiplicationKey" @click="addToDisplay(`*`)">*</button>
+    <button id="divisionKey" @click="addToDisplay(`/`)">/</button>
+    <button id="equalKey" @click="addToDisplay(`=`)">=</button>
+    <button id="resetKey" @click="addToDisplay(`c`)" class="resetButton">C</button>
+  </main></template>
 
 <style scoped>
 
